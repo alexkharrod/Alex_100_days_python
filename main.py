0730cc688406209a553bea75becfa8f1ce090297
alexharrod <alexkharrod@gmail.com>
2024-09-14 06:35:43 -0400

    about to leave for HT - next lesson is dymanic typing

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+import math
+from tkinter import *
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    pass
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(300)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro Timer")
+window.config(padx=100, pady=50, bg=YELLOW)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME,35, "bold"))
+canvas.grid(column=1, row=1)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+timer_label = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(column=1, row = 0)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+checkmark_label = Label(text="✓", fg=GREEN, bg=YELLOW)
+checkmark_label.grid(column=1, row=3)
+
+start_button = Button(text="Start", command=start_timer, highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", command=reset_timer, highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+
+
+
+
+mainloop()
